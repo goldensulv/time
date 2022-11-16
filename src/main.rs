6f74fc26872da469e3c11d707cb0e41db1115a2c
Shalev Goldfarb <goldensulv@gmail.com>
2022-11-16 02:02:45 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,19 @@ struct Cli {
     times: i32,
 }
 
+fn parse_time(time: Duration) -> String {
+    format!(
+        "{} days, {:>2} hrs {:>2} min {:>2} sec {:>3} ms {:>3} us {:>3} ns",
+        time.as_secs() / 86400,
+        time.as_secs() / 3600,
+        time.as_secs() / 60,
+        time.as_secs() % 60,
+        time.subsec_millis(),
+        time.subsec_micros() % 1000,
+        time.subsec_nanos() % 1000
+    )
+}
+
 fn main() -> io::Result<()> {
     let args = Cli::parse();
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
@@ -26,22 +39,12 @@ fn main() -> io::Result<()> {
             Ok(mut child) => {
                 let result = child.wait();
                 if let Ok(status) = result {
-                    let time = time::Instant::now().duration_since(start);
+                    let time = start.elapsed();
                     average_time += time;
                     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Magenta)))?;
                     println!("\nProcess finished. {}", status);
-                    let time = format!(
-                        "{} days, {:0>2} hrs {:0>2} min {:0>2} sec {:>3} ms {:>3} us {:>3} ns",
-                        time.as_secs() / 86400,
-                        time.as_secs() / 3600,
-                        time.as_secs() / 60,
-                        time.as_secs() % 60,
-                        time.subsec_millis(),
-                        time.subsec_micros() % 1000,
-                        time.subsec_nanos() % 1000
-                    );
                     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
-                    println!("Time: {}", time);
+                    println!("Time: {}", parse_time(time));
                 }
             }
             Err(e) => {
@@ -55,18 +58,8 @@ fn main() -> io::Result<()> {
 
     if !error {
         average_time /= args.times as u32;
-        let time = format!(
-            "{} days, {:0>2} hrs {:0>2} min {:0>2} sec {:>3} ms {:>3} us {:>3} ns",
-            average_time.as_secs() / 86400,
-            average_time.as_secs() / 3600,
-            average_time.as_secs() / 60,
-            average_time.as_secs() % 60,
-            average_time.subsec_millis(),
-            average_time.subsec_micros() % 1000,
-            average_time.subsec_nanos() % 1000
-        );
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
-        println!("\nAverage time for {} runs: {}", args.times, time);
+        println!("\nAverage time for {} runs: {}", args.times, parse_time(average_time));
     }
 
     stdout.reset()?;
